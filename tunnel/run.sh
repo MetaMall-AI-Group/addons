bceb0ba4562b13958c4d127631da5e2d7772b0be
imdgr886 <thrall.chen@gmail.com>
2022-11-16 21:44:31 +0800

    Update run.sh

--- tunnel/run.sh
+++ tunnel/run.sh
@@ -8,13 +8,15 @@ TOKEN=$(jq --raw-output ".frp_token" $CONFIG_PATH)
 SERVER_LOCAL=$(jq --raw-output ".local_host" $CONFIG_PATH)
 PORT_LOCAL=$(jq --raw-output ".local_port" $CONFIG_PATH)
 HTTP_DOMAIN=$(jq --raw-output ".http_domain" $CONFIG_PATH)
+WEB_NAME = $(cat /proc/sys/kernel/random/uuid)
 
 echo "" > /frpc.ini
 echo "[common]" >> /frpc.ini
 echo "server_addr = ${HOST}" >> /frpc.ini
 echo "server_port = ${PORT}" >> /frpc.ini
 echo "jwt_token = ${TOKEN}" >> /frpc.ini
-echo "[home-assistant]" >> /frpc.ini
+echo "" >> /frpc.ini
+echo "[${WEB_NAME}]" >> /frpc.ini
 echo "type = http" >> /frpc.ini
 echo "local_ip = ${SERVER_LOCAL}" >> /frpc.ini
 echo "local_port = ${PORT_LOCAL}" >> /frpc.ini
